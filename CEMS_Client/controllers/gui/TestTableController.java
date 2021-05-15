a65086949023d96419b05c773d35b5db9bb5230a
menison <meni.shitrit@gmail.com>
2021-05-15 18:59:52 +0300

    aaaaaaaaaaaaaaaa

---
+++ CEMS_Client/controllers/gui/TestTableController.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TableView;
+
+public class TestTableController {
+	
+	@FXML
+	private TableView tblTests;
+	@FXML
+	private Button btnBack;
+	@FXML
+	private Button btnRequestCopy;
+
+	
+}
