4a23cb5ed87257fdf6cac671afd9f1bb9c811e96
David <David@DESKTOP-2SAK2VK>
2021-05-16 15:35:38 +0300

    added controllers

--- CEMS_Client/controllers/gui/AddQuestionForTestController.java
+++ CEMS_Client/controllers/gui/AddQuestionForTestController.java
@@ -1,4 +1,54 @@
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class AddQuestionForTestController {
+
+    @FXML
+    private JFXButton btnSearch;
+
+    @FXML
+    private JFXButton btnClose;
+
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private TableView<?> questionTable;
+
+    @FXML
+    private TableColumn<?, ?> qustionIDcol;
+
+    @FXML
+    private TableColumn<?, ?> subjectCol;
+
+    @FXML
+    private TableColumn<?, ?> CourseCol;
+
+    @FXML
+    private TableColumn<?, ?> composerCol;
+
+    @FXML
+    private JFXTextField courseNameField;
+
+    @FXML
+    private JFXTextField pointsField;
+
+    @FXML
+    void btnAddClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCloseClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchClicked(ActionEvent event) {
+
+    }
 
 }
--- CEMS_Client/controllers/gui/AddTestController.java
+++ CEMS_Client/controllers/gui/AddTestController.java
@@ -1,4 +1,67 @@
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class AddTestController {
+
+    @FXML
+    private JFXButton btnAddQuestion;
+
+    @FXML
+    private JFXButton btnDeleteQuestion;
+
+    @FXML
+    private JFXButton btnClose;
+
+    @FXML
+    private JFXButton btnSubmit;
+
+    @FXML
+    private TableView<?> QuestionsTable;
+
+    @FXML
+    private TableColumn<?, ?> QuestionIDCol;
+
+    @FXML
+    private TableColumn<?, ?> PointsCol;
+
+    @FXML
+    private JFXTextField DurationField;
+
+    @FXML
+    private JFXComboBox<?> chooseSubjectBox;
+
+    @FXML
+    private JFXComboBox<?> chooseCourseBox;
+
+    @FXML
+    private JFXTextArea TeacherCommentsField;
+
+    @FXML
+    private JFXTextArea StudentCommentsField;
+
+    @FXML
+    void btnAddQuestionClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCloseClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteQuestionClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSubmitClicked(ActionEvent event) {
+
+    }
 
 }
