14c75e443915911a3fcabb37e5b9743b1c073750
David <David@DESKTOP-2SAK2VK>
2021-05-16 15:35:08 +0300

    a

---
+++ CEMS_Client/controllers/gui/ActivateTestController.java
@@ -0,0 +1,26 @@
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+public class ActivateTestController {
+
+    @FXML
+    private JFXTextField pinCodeField;
+
+    @FXML
+    private JFXButton btnActivate;
+
+    @FXML
+    private JFXButton btnClose;
+
+    @FXML
+    void btnActivateClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCloseClicked(ActionEvent event) {
+
+    }
+
+}
