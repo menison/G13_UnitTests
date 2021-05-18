07ed82418c7f33ba6030da0711bb802f847ecd70
mattb12321 <matti.manof@gmail.com>
2021-05-18 22:52:52 +0300

    Added TestQuestionMiddle with Controller

---
+++ CEMS_Client/controllers/gui/TestQuestionMiddleController.java
@@ -0,0 +1,35 @@
+package gui;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.RadioButton;
+
+public class TestQuestionMiddleController {
+
+    @FXML
+    private Button middleQuestion_btnNext;
+
+    @FXML
+    private RadioButton middleQuestion_radioAns1;
+
+    @FXML
+    private RadioButton middleQuestion_radioAns2;
+
+    @FXML
+    private RadioButton middleQuestion_radioAns4;
+
+    @FXML
+    private RadioButton middleQuestion_radioAns3;
+
+    @FXML
+    private ProgressBar middleQuestion_progressBar;
+
+    @FXML
+    private JFXTextField middleQuestion_pieProgressIndic;
+
+    @FXML
+    private Button middleQuestion_back;
+
+}
