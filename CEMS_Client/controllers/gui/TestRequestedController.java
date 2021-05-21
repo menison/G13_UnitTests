30675eeaa9f57319e5addc18f2da570d972a4839
mattb12321 <matti.manof@gmail.com>
2021-05-21 11:35:01 +0300

    Updated TestRequested and TestTable fxml and Controllers

--- CEMS_Client/controllers/gui/TestRequestedController.java
+++ CEMS_Client/controllers/gui/TestRequestedController.java
@@ -1,11 +1,38 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class TestRequestedController {
-	@FXML
-	private Button TestCopySuccess_btnClose;
+
+    @FXML
+    private Button TestCopySuccess_btnClose;
+
+    @FXML
+    void Close(ActionEvent event) {
+    	Stage stage = (Stage) TestCopySuccess_btnClose.getScene().getWindow();
+    	stage.close();
+    }
+    
+    public void start(Stage primaryStage) throws Exception {	
+		//Parent root = FXMLLoader.load(getClass().getResource("TestRequested.fxml"));
+    	Pane root;
+    	FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("TestRequested.fxml"));
+		root = loader.load();
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Test Requested");
+		primaryStage.setScene(scene);
+		primaryStage.show();	
+	}
 
 }
--- CEMS_Client/controllers/gui/TestTableController.java
+++ CEMS_Client/controllers/gui/TestTableController.java
@@ -1,32 +1,69 @@
 package gui;
 
-import entities.Exam;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-
-import javafx.scene.control.TableView;
-
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class TestTableController {
-	@FXML
-	private TableView<Exam> myTestsTable_tblTests;
-	@FXML
-	private TableColumn<Exam, String> myTests_colTestID;
-	@FXML
-	private TableColumn<Exam, String> myTests_colTestCode;
-	@FXML
-	private TableColumn<Exam, String> myTests_colDate;
-	@FXML
-	private TableColumn<Exam, String> myTests_colAllocDuration;
-	@FXML
-	private TableColumn<Exam, String> myTests_colActualDuration;
-	@FXML
-	private TableColumn<Exam, String> myTests_colGrade;
-	@FXML
-	private Button myTestsTable_btnBack;
-	@FXML
-	private Button myTestsTable_btnRequestCopy;
 
+    @FXML
+    private TableView<?> myTestsTable_tblTests;
+
+    @FXML
+    private TableColumn<?, ?> myTests_colTestID;
+
+    @FXML
+    private TableColumn<?, ?> myTests_colTestCode;
+
+    @FXML
+    private TableColumn<?, ?> myTests_colDate;
+
+    @FXML
+    private TableColumn<?, ?> myTests_colAllocDuration;
+
+    @FXML
+    private TableColumn<?, ?> myTests_colActualDuration;
+
+    @FXML
+    private TableColumn<?, ?> myTests_colGrade;
+
+    @FXML
+    private Button myTestsTable_btnBack;
+
+    @FXML
+    private Button myTestsTable_btnRequestCopy;
+    
+    @FXML
+    void requestTestCopy(ActionEvent event) throws Exception {
+    	Stage newStage = new Stage();
+    	TestRequestedController trc = new TestRequestedController();
+		trc.start(newStage);
+    }
+    
+    @FXML
+    void back(ActionEvent event) throws Exception {
+    	Stage newStage = new Stage();
+    	Stage stage = (Stage) myTestsTable_btnBack.getScene().getWindow();
+    	StudentMenuController smc = new StudentMenuController();
+    	smc.start(newStage);
+    	stage.close();
+    }
+    
+    public void start(Stage primaryStage) throws Exception {	
+		//Parent root = FXMLLoader.load(getClass().getResource("TestRequested.fxml"));
+    	Pane root;
+    	FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("TestTable.fxml"));
+		root = loader.load();
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Test Table");
+		primaryStage.setScene(scene);
+		primaryStage.show();	
+	}
 }
