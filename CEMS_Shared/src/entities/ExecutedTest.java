b0e171ed201138fe3843c1f3925eba6eedca6aed
menison <meni.shitrit@gmail.com>
2021-05-22 16:51:22 +0300

    by meni

--- CEMS_Shared/src/entities/ExecutedTest.java
+++ CEMS_Shared/src/entities/ExecutedTest.java
@@ -1,8 +1,16 @@
 package entities;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ExecutedTest extends Test {
+	public ExecutedTest(ArrayList<Question> questions, String testID, int allocatedDuration, String commentsForStudents,
+			String commentsForTeachers, String currExecutionCode, HashMap<Question, Integer> pointDistribution,
+			boolean isActivated) {
+		super(questions, testID, allocatedDuration, commentsForStudents, commentsForTeachers, currExecutionCode,
+				pointDistribution, isActivated);
+	}
+
 	private String executionCodePK;
 
 
