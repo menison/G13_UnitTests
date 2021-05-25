fc4dfe7f22f3c76d463cf10f0396359247045c43
Aviv <Aviv@DESKTOP-8CMJ1T0>
2021-05-25 20:38:10 +0300

    Changed data parsing

--- CEMS_Server/src/dataParsing/DataParsing.java
+++ CEMS_Server/src/dataParsing/DataParsing.java
@@ -1,17 +1,9 @@
 package dataParsing;
 
 import common.Permission;
+import database.GetLogin;
 import entities.Message;
 import server.EchoServer;
-
-import sql.GetExistsOrders;
-import sql.GetChangeList;
-import sql.GetLogin;
-import sql.GetNewFamily;
-import sql.GetNewGuide;
-import sql.GetOrder;
-import sql.GetReport;
-import sql.OrderCancel;
 
 /**
  * A function that includes all the cases in which 
@@ -29,109 +21,13 @@ public class DataParsing {
 			return null;
 		}
 		switch (receivedMessage.getOperationType()) {
-		case WorkerLogin:
-			return GetLogin.WorkerLoginData(receivedMessage);
-
-		case VisitorLogin:
-			return GetLogin.VisitorLoginData(receivedMessage);
-
-		case CancellationReport:
-			return GetReport.CancellationReportData(receivedMessage);
-
-		case VisitorReport:
-			return GetReport.VisitorReportData(receivedMessage);
-
-		case TotalVisitorReport:
-			return GetReport.TotalVisitorReportData(receivedMessage);
-
-		case UsageReport:
-			return GetReport.UsageReportData(receivedMessage);
-
-		case MonthlyIncomeReport:
-			return GetReport.MonthlyIncomeData(receivedMessage);
-
-		case GetPark:
-			return GetOrder.GetPark(receivedMessage);
-
-		case addNewGuide:
-			return GetNewGuide.NewGuideData(receivedMessage);
-
-		case addNewFamily:
-			return GetNewFamily.NewFamilyData(receivedMessage);
-		case GetAllParksName:
-			return GetOrder.GetAllParkNames();
-		case SetChangInPark:
-			return GetChangeList.SetList(receivedMessage);
-		case GetChangInPark:
-			return GetChangeList.GetList(receivedMessage);
-		case ApplyChangInPark:
-			return GetChangeList.ApplyList(receivedMessage);
-		case DeleteChangInPark:
-			return GetChangeList.DeleteList(receivedMessage);
-		case OrderTimeValidation:
-			return GetOrder.PreOrderTimeValidation(receivedMessage);
-
-		case getDiscountOfPark:
-			return GetOrder.GetDiscountOfPark(receivedMessage);
-
-		case AlternativeDates:
-			return GetOrder.GetAlternativeDates(receivedMessage);
-
-		case GetWaitingListIndex:
-			return GetOrder.GetWaitingListIndex(receivedMessage);
-
-		case NewOrder:
-			return GetOrder.SetNewOrder(receivedMessage);
-
-		case existsOrders:
-			return GetExistsOrders.OrdersData(receivedMessage);
-
-		case OrdersInWaitingList:
-			return GetExistsOrders.OrdersInWaitingListData(receivedMessage);
-
-		case CancelOrderByUser:
-			return OrderCancel.CancelOrder(receivedMessage);
-
-		case CancelOrderFromWaitingList:
-			return OrderCancel.CancelOrderFromWaitingList(receivedMessage);
-
-		case ExitFromWaitingListApproval:
-			return GetOrder.GetOrderOutFromWaitingList(receivedMessage);
-
-		case ApprovedOrder:
-			return GetOrder.ApprovedOrder(receivedMessage);
-
-		case AllOrdersMonth:
-			return GetReport.getAllMonthFromOrders(receivedMessage);
-			
-		case AllOrdersMonthCancellation:
-			return GetReport.getAllOrdersMonthCancellation(receivedMessage);
 		
-		case VisitorPerDateReport:
-			return GetReport.getVisitorPerDateReport(receivedMessage);
-
-		case OrderTimeValidationNotPreOrder:
-			return GetOrder.OrderTimeValidation(receivedMessage);
-			
-			
-		case SpecificNumberOfVisitors:
-			return GetVisitors.GetSpecificNumberOfVisitors(receivedMessage);
-			
-		case SubNumberLogin:
-			return GetLogin.SubLoginData(receivedMessage);
-
-		case OrdersById:
-			return GetOrder.GetOrdersById(receivedMessage);
-		case SetNewStatusOrder:
-			return GetOrder.SetNewStatusOrder(receivedMessage);
-		case NumberOfVisitors:
-			return GetVisitors.GetNumberOfVisitors(receivedMessage);
-		case LoggedOut:
-			return GetLogin.logOut(receivedMessage);
-
-
+		case Login:
+			return GetLogin.userLoginData(receivedMessage);
 		default:
 			break;
+				
+		
 		}
 		return null;
 	}
