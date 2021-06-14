fd3ba5c5f2fa12400993246b5bbcb21cd0eecbc6
Aviv <Aviv@DESKTOP-8CMJ1T0>
2021-06-14 07:56:53 +0300

    Added few javadoc comments

--- CEMS_Client/controllers/request/AbstractController.java
+++ CEMS_Client/controllers/request/AbstractController.java
@@ -4,10 +4,10 @@ import cachedUserData.DataManager;
 import application.ClientUI;
 import entities.Message;
 
-/**
- * @author avivs100
- *	The master class for all controllers designed to transfer 
- *  information between the user and the server
+
+/**Abstract class with the purpose of sending information to the server
+ * @author Aviv
+ *
  */
 public abstract class AbstractController {
 	public static DataManager dataManager = DataManager.getDataManager();
