25d1972b2e205a63cc0b67948a2bb34a7e9cafab
mattb12321 <matti.manof@gmail.com>
2021-06-14 09:04:31 +0300

    Manual test timer updated

--- CEMS_Client/controllers/gui/ManualTestController.java
+++ CEMS_Client/controllers/gui/ManualTestController.java
@@ -2,6 +2,11 @@ package gui;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import com.jfoenix.controls.JFXTextField;
 import application.ClientUI;
 import cachedUserData.DataManager;
@@ -9,9 +14,12 @@ import common.Operation;
 import entities.ExecutedTest;
 import entities.Message;
 import entities.Test;
+import gui.TestQuestionMiddleController.App;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressIndicator;
@@ -20,7 +28,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
-public class ManualTestController {
+public class ManualTestController implements Initializable{
 
 	private File SolutionFile;
 	
@@ -40,7 +48,9 @@ public class ManualTestController {
 
 	@FXML
 	private Button manualTest_btnSend;
-
+	
+	private boolean submited;
+	private ExecutedTest execTest;
 	@FXML
     void downloadTest(ActionEvent event) {
 		Stage stage = (Stage) manualTest_btnDownload.getScene().getWindow();
@@ -90,6 +100,7 @@ public class ManualTestController {
 		Pane root;
     	FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass().getResource("/fxml/ManualTest.fxml"));
+		submited = false;
 		try {
 			root = loader.load();
 			Scene scene = new Scene(root);
@@ -100,4 +111,38 @@ public class ManualTestController {
 			e.printStackTrace();
 		}
 	}
+	
+	class App extends TimerTask {
+    	int allocatedDuration = execTest.getTest().getAllocatedDuration();
+        int countdown = allocatedDuration;
+        int countUp = 0;
+        int totalSeconds = allocatedDuration*60;
+        long h,m,s;
+        public void run() { 
+        	s = totalSeconds % 60;
+        	h = totalSeconds / 60;
+        	m = h % 60;
+        	h/=60;
+        	Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                	manualTest_pieProgressIndicator.setProgress((double)countUp/(allocatedDuration*60));
+                	manualTest_txtElapsedTime.setText(h+":"+m+":"+s);
+                }
+            });
+            countUp++;
+            if(submited) 
+            	this.cancel();
+        	totalSeconds--;
+        }
+    }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+    	DataManager dm = DataManager.getDataManager();
+    	execTest = dm.getTestInExecution();
+    	Timer timer = new Timer();
+    	timer.schedule(new App(), 0, 1000);
+	}
+
 }
