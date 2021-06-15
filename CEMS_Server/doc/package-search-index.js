afeb820fcb219ccffd95c4d291bf4eaf686eaad8
menison <meni.shitrit@gmail.com>
2021-06-15 23:09:29 +0300

    asdasdsadasdasd

---
+++ CEMS_Server/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"application"},{"l":"database"},{"l":"dataParsing"},{"l":"gui"},{"l":"logics"},{"l":"server"}];updateSearchResults();
